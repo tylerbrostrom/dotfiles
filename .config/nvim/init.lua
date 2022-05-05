c2a76d9abf0f6b977a00b26ef0a44271a8f23603
Tyler Brostrom <5232237+tylerbrostrom@users.noreply.github.com>
2022-05-05 16:04:16 -0500

    set up packer.nvim, split up nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,2 @@
-local o = vim.o
-
-o.termguicolors = true
-
--- Number of screen lines to keep above and below the cursor
-o.scrolloff = 8
-
--- Better editor UI
-o.number = true
-o.numberwidth = 6
-o.relativenumber = true
-o.signcolumn = 'yes'
-o.cursorline = true
-
--- Indent
-o.wrap = false
-o.autoindent = true
-o.smartindent = true
-o.tabstop = 4
-o.shiftwidth = 4
-o.expandtab = true
+require "user.options"
+require "user.plugins"
